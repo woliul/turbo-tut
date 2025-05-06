688f7b05e94e274de422e891d97edbc3d4f3f2f9
Woliul <hmwoliul@gmail.com>
2025-05-07 02:30:38 +0600

    Add animal list management with access and update functionality

---
+++ Week 1/day1/pr115 Lists.py
@@ -0,0 +1,11 @@
+# Define a list of animals
+animals_list = ["cat", "dog", "tiger", "elephant"]
+
+# Accessing an item
+print("First animal:", animals_list[0])
+
+# Adding a new animal
+animals_list.append("lion")
+
+# Printing the list
+print("Updated animal list:", animals_list)
